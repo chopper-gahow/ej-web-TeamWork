9d5169a6c7d981184e6ce880941f4ff56f5b8a6c
chopper-gahow <1052047390@qq.com>
2020-01-04 11:14:54 +0800

    Test1

--- src/pages/address/list.vue
+++ src/pages/address/list.vue
@@ -1,4 +1,155 @@
 <template>
-    <div>XX管理</div>
-    <!-- yangge -->
+    <div>地址管理
+     <el-button type="success" @click="toAddHandler">添加</el-button>
+    <el-button type="danger" >删除</el-button>
+    <el-table :data="addresses">
+            <el-table-column  prop="id" label="编号"></el-table-column>
+            <el-table-column prop="province" label="省份"></el-table-column>
+            <el-table-column prop="city" label="城市"></el-table-column>
+            <el-table-column prop="area" label="区"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+            <el-table-column prop="telephone" label="联系方式"></el-table-column>
+            <el-table-column prop="customerId" label="顾客号"></el-table-column>
+            <el-table-column label="操作">
+                <template v-slot="slot">
+                    <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
+                    <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
+                </template>
+            </el-table-column>
+
+        </el-table>
+        <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+        <el-dialog title="录入地址信息" :visible.sync="visible" width="60%" >
+         <span>
+           
+             <el-form :model="from" label-width=80px>
+                <el-form-item label="省份">
+                    <!-- v-model双向数据绑定 -->
+                    <el-input v-model="form.province">
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="城市">
+                    <el-input v-model="form.city"></el-input>
+                </el-form-item>
+                <el-form-item label="区">
+                    <el-input  v-model="form.area"></el-input>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input  v-model="form.address"></el-input>
+                </el-form-item>
+                <el-form-item label="联系方式">
+                    <el-input  v-model="form.telephone"></el-input>
+                </el-form-item>
+                <el-form-item label="顾客号">
+                    <el-input  v-model="form.customerId"></el-input>
+                </el-form-item>
+             </el-form>
+             </span>
+         <span slot="footer" class="dialog-footer">
+         <el-button @click="visible = false">取 消</el-button>
+         <!-- @click=onclick -->
+         <el-button type="primary" @click="submitHandler">确 定</el-button>
+         </span>
+        </el-dialog>
+    </div>
 </template>
+
+<script>
+import request from '@/utils/request'
+import querystring from 'querystring'
+//暴露接口
+export default {
+    // 存放网页中需要调用的方法
+  methods:{
+        loadData(){
+      let url="http://localhost:6677/address/findAll"
+      request.get(url).then((response)=>{
+        // 将查询结果设置到customers中，this指向外部函数的this
+        this.addresses = response.data;
+      })
+    },
+    submitHandler(){
+  
+      let url = "http://localhost:6677/address/saveOrUpdate";
+      request({
+        url,
+        method:"POST",
+        headers:{
+          "Content-Type":"application/x-www-form-urlencoded"
+        },
+        data:querystring.stringify(this.form)
+      }).then((response)=>{
+        // 模态框关闭
+        this.closeModalHandler();
+        // 刷新
+        this.loadData();
+        // 提示消息
+        this.$message({
+          type:"success",
+          message:response.message
+        })
+      })
+
+    },
+    toDeleteHandler(id){
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let url="http://localhost:6677/address/deleteById?id="+id;
+        // let that=this;
+        request.get(url).then((response)=>{
+            this.loadData();
+        })
+        this.$message({
+        type: 'success',
+        message: response.message
+        });
+      })
+      
+    },
+    toUpdateHandler(row){
+      this.visible = true;
+      this.form=row;
+    },
+    closeModalHandler(){
+      this.visible = false;
+    },
+    toAddHandler(){
+      this.visible = true;
+      this.form={
+        type:"address"
+      }
+
+    }
+
+  },
+  // 用于存放要向网页中显示的数据
+    //用于存放要在网页中存放的数据
+    data(){
+        return {
+            form:{
+                type:"address"
+            },
+            visible:false,
+            addresses:[]
+        }
+
+    },
+    created(){
+        let url="http://localhost:6677/address/findAll"
+        // let that = this
+        request.get(url).then((response)=>{
+            //将查询结果设置到customers中
+            this.addresses = response.data;
+        })
+        this.loadData()
+        
+    }
+
+}
+</script>
+<style scoped>
+       
+</style>
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
-
+//修改后
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
@@ -10,7 +10,7 @@ const service = axios.create({
   timeout: 5000 // request timeout
 })
 
-// request interceptor
+// request interceptor(拦截器)
 service.interceptors.request.use(
   config => {
     // do something before request is sent
@@ -44,32 +44,7 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
-    }
+    return res;
   },
   error => {
     console.log('err' + error) // for debug
